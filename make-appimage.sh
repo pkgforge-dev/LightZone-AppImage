e851b56ffdbb87fb3b79a8f03c9112690497fa05
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-05 07:59:49 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,25 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q lightzone | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/256x256/apps/lightzone.png
+export DESKTOP=/usr/share/applications/lightzone.desktop
+export DEPLOY_P11KIT=1
+export DEPLOY_COMMON_LIBS=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/lightzone \
+	/usr/lib/lightzone \
+	/usr/bin/dcraw_lz  \
+	/usr/bin/java      \
+	/usr/share/java
 
-# Additional changes can be done in between here
+sed -i -e 's|usrdir=/usr|usrdir=$APPDIR|' ./AppDir/bin/lightzone
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
